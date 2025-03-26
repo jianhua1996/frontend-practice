1b463a965a2b91ccde76071d630ecd836f71d5ee
chen <1778868778@qq.com>
2025-03-26 09:54:16 +0800

    优化大文件分片代码结构

--- src/utils/file.ts
+++ src/utils/file.ts
@@ -1,4 +1,4 @@
-interface Chunk {
+export interface Chunk {
 	name: string
 	size: number
 	start: number
@@ -23,12 +23,49 @@ export function createChunk(file: File, start: number, end: number): Chunk {
 	}
 }
 
+interface workerOptions {
+	file: File
+	startTaskIndex: number
+	endTaskIndex: number
+	chunkSize: number
+	onMessage: (event: MessageEvent) => void
+	onError: (error: ErrorEvent) => void
+}
+function startWorker({ file, startTaskIndex, endTaskIndex, chunkSize, onMessage, onError }: workerOptions): Worker {
+	const worker = new Worker(new URL('./worker.ts', import.meta.url), { type: 'module' })
+
+	// 传递参数给线程
+	worker.postMessage({
+		file,
+		startTaskIndex,
+		endTaskIndex,
+		chunkSize
+	})
+
+	worker.onmessage = onMessage
+	worker.onerror = onError
+
+	return worker
+}
+
+export interface progressType {
+	type: 'start' | 'progress' | 'complete'
+	file?: { name: string; size: number }
+	sliceInfo?: {
+		chunkSize: number
+		chunkCount: number
+		threadNum: number
+	}
+	current?: number
+	total?: number
+}
+
 interface CutFileOptions {
 	chunkSize?: number
 	threadNum?: number
-	onProgress?: Function
+	onProgress?: (progress: progressType) => void
 }
-export function cutFile(file: File, options: CutFileOptions = {}) {
+export function cutFile(file: File, options: CutFileOptions = {}): Promise<Chunk[]> {
 	return new Promise((resolve, reject) => {
 		if (!file || !(file instanceof File)) {
 			throw new Error('file parameter must be a File instance')
@@ -37,11 +74,8 @@ export function cutFile(file: File, options: CutFileOptions = {}) {
 		let { chunkSize = 1024 * 1024, threadNum = navigator.hardwareConcurrency || 4, onProgress } = options
 
 		// 参数校验增强
-		if (chunkSize <= 0) {
-			throw new Error('chunkSize must be a positive number')
-		}
-		if (threadNum <= 0) {
-			threadNum = Math.max(1, navigator.hardwareConcurrency || 4)
+		if (chunkSize <= 0 || threadNum <= 0) {
+			throw new Error('chunkSize and threadNum must be greater than 0')
 		}
 
 		if (onProgress && typeof onProgress !== 'function') throw new Error('onProgress must be a function')
@@ -51,19 +85,23 @@ export function cutFile(file: File, options: CutFileOptions = {}) {
 		const minTaskOfThread = Math.floor(chunkCount / threadNum) // 任务数除以线程数，向下取整，得到每个线程至少要执行的任务数
 		let restTaskOfThread = chunkCount % threadNum // 任务数除以线程数，取余，得到剩余的任务数
 
-		const chunkTasks = [] // 准备一个数组，用来存储所有结束的任务
+		const chunkTasks: Chunk[] = [] // 准备一个数组，用来存储所有结束的任务
 		let currentStartTaskIndex = 0 // 当前线程开始执行的任务索引
 		let dispatchedAll = false // 是否已经分发完所有任务
 		let completedTasks = 0
-		const workers = []
+		const workers: Worker[] = []
 
 		onProgress?.({
 			type: 'start',
-			fileName: file.name,
-			fileSize: file.size,
-			chunkSize: chunkSize,
-			chunkCount,
-			threadNum
+			file: {
+				name: file.name,
+				size: file.size
+			},
+			sliceInfo: {
+				chunkSize,
+				chunkCount,
+				threadNum
+			}
 		})
 
 		for (let i = 0; i < threadNum; i++) {
@@ -89,46 +127,46 @@ export function cutFile(file: File, options: CutFileOptions = {}) {
 			}
 
 			// 创建一个线程，并传递参数
-			const worker = new Worker(new URL('./worker.ts', import.meta.url), { type: 'module' })
-			workers.push(worker)
-
-			// 传递参数给线程
-			worker.postMessage({
+			const worker = startWorker({
 				file,
 				startTaskIndex,
 				endTaskIndex,
-				chunkSize
-			})
+				chunkSize,
+				onMessage: e => {
+					for (let j = 0; j < e.data.length; j++) {
+						const tIndex = startTaskIndex + j
+						if (chunkTasks[tIndex]) {
+							console.warn(`Chunk ${tIndex} has already been processed.`)
+						} else {
+							completedTasks++ // 完成的任务数加1
+						}
+						chunkTasks[tIndex] = e.data[j]
+					}
 
-			worker.onmessage = e => {
-				for (let j = 0; j < e.data.length; j++) {
-					chunkTasks[startTaskIndex + j] = e.data[j]
-				}
-
-				completedTasks += e.data.length
-				onProgress?.({
-					type: 'progress',
-					current: completedTasks,
-					total: chunkCount
-				})
-
-				if (chunkTasks.length === chunkCount) {
-					resolve(chunkTasks)
 					onProgress?.({
-						type: 'complete',
+						type: 'progress',
 						current: completedTasks,
 						total: chunkCount
 					})
+
+					if (completedTasks === chunkCount) {
+						resolve(chunkTasks)
+						onProgress?.({
+							type: 'complete',
+							current: completedTasks,
+							total: chunkCount
+						})
+					}
+
+					worker.terminate()
+				},
+				onError: err => {
+					reject(new Error(`Worker error: ${err.message}`))
+					workers.forEach(worker => worker.terminate())
+					workers.length = 0
 				}
-
-				worker.terminate()
-			}
-
-			worker.onerror = err => {
-				reject(new Error(`Worker error: ${err.message}`))
-				workers.forEach(worker => worker.terminate())
-				workers.length = 0
-			}
+			})
+			workers.push(worker)
 		}
 	})
 }
--- src/views/FileSlice.vue
+++ src/views/FileSlice.vue
@@ -11,32 +11,30 @@
 		/>
 	</div>
 </template>
-<script setup>
-import { cutFile } from '@/utils/file.ts'
+<script setup lang="ts">
+import { cutFile, progressType } from '@/utils/file'
 
 const logContent = ref('')
 
-const handleProgress = e => {
-	// console.log(e)
+const handleProgress = (e: progressType) => {
+	if (e.type === 'start') {
+		logContent.value += `开始执行，总文件大小：${e.file.size}，文件名：${e.file.name}，分片数：${e.sliceInfo.chunkCount}，线程数：${e.sliceInfo.threadNum}\n\n`
+	}
 	if (e.type === 'progress') {
 		logContent.value += `进度： ${((e.current / e.total) * 100).toFixed(2)}%， 当前执行： ${e.current}/${e.total}\n`
 	}
+	if (e.type === 'complete') {
+		logContent.value += `完成\n\n`
+	}
 }
-const handleFileChange = async e => {
-	const file = e.target.files[0]
+const handleFileChange = async (e: InputEvent) => {
+	const file = (e.target as HTMLInputElement).files[0]
 	const chunks = await cutFile(file, { onProgress: handleProgress })
-	// console.log(chunks)
-	// downloadFile(chunks, { name: file.name, type: file.type })
+
+	downloadFile(chunks, { name: file.name, type: file.type })
 }
 
 const downloadFile = (chunks, { name, type }) => {
-	// {
-	//     "name": "Downloads_2.zip",
-	//     "size": 1048576,
-	//     "start": 9437184,
-	//     "end": 10485760,
-	//     "data": {}  // Blob
-	// }
 	chunks.sort((a, b) => a.start - b.start)
 
 	const blob = new Blob(
